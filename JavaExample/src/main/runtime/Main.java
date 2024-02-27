513dc3b71a8e17c569b1fd50aeca98041584366a
Lachlan Ellis <37792607+lellis0406@users.noreply.github.com>
2024-02-28 09:57:00 +1000

    additional comments

--- JavaExample/src/main/runtime/Main.java
+++ JavaExample/src/main/runtime/Main.java
@@ -1,6 +1,5 @@
 package main.runtime;
 
-import main.lib.houses.House;
 import main.lib.people.types.Insomniac;
 import main.lib.people.Person;
 import main.lib.people.types.Surgeon;
@@ -15,7 +14,7 @@ import main.lib.people.types.Surgeon;
 //and I will be using them in this project to talk about things
 
 //they don't actually do anything in the code; they are just a way for you to add
-//details for people reading
+//details for people looking at your code to read
 
 //-------------------------------------------------------------------------------
 
@@ -29,16 +28,22 @@ public class Main {
         Person john;
         //you have told the compiler there is a person named
         //john, but you haven't actually created john yet
+        //this is called "declaration" or "declaring" an object
+        //if you tried to do anything with john at this point
+        //the program will throw an exception and stop
 
         Person james = new Surgeon();
         //you have told the compiler that there is a person
         //named james, and you have created them as a Surgeon
+        //this is called "instantiating" or creating an instance of an object
+        //you are now allowed to do anything you could do
+        //with a surgeon (see surgeon type in lib.people.types) to james
 
         john = new Insomniac();
-        //you went back and told the compiler that john
-        //is an insomniac
+        //you went back and instantiated
+        //john as an Insomniac
 
-        //it's important to note that ALL non primitive or derived types (google it)
+        //it's important to note that ALL non primitive or derived types (google these)
         //require that you use the new keyword as above when instantiating.
 
         //---------------------------------------------------------------------------
@@ -53,8 +58,33 @@ public class Main {
         }
         */
 
-        while(john.sleep() == 100.0f) {// this is false, so it will never start looping
-            System.out.println("Boom!!");//(it won't happen)
+        if(james.work() == 3.0f) {
+            //won't happen
+        } else { //if the first statement isn't true
+            System.out.println("normal day of work");
+            //this only happens if the first statement is NOT true
+        }
+
+        /* format:
+        if(<condition>) {
+          <do this>
+        } else if(<condition>) {
+          <do this>
+        } else if(<condition>) {
+          <do this>
+        .....
+        } else {
+            <do if no prior condition is filled>
+        }
+        */
+        //yes, you CAN chain elses and ifs, but doing it too much looks ugly
+        //and is difficult to read, so chain elses in moderation
+        //lest you end up like this guy
+        //https://imgur.com/a/FIlpWC2
+
+
+        while(john.sleep() == 100.0f) { //this is false, so it will never start looping
+            System.out.println("Boom!!"); //(it won't happen)
         }
 
         /* format:
@@ -72,6 +102,8 @@ public class Main {
         Insomniac jane = null;
         jane.work();
         */
+
+        //^ null pointer exception example, uncomment and compile/run to see
 
     }
 }
