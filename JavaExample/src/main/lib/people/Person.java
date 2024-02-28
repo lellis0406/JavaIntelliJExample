39623b08e6bb3b647314538b3f655e00555bdb67
Lachlan Ellis <37792607+lellis0406@users.noreply.github.com>
2024-02-28 10:36:55 +1000

    person

--- JavaExample/src/main/lib/people/Person.java
+++ JavaExample/src/main/lib/people/Person.java
@@ -8,4 +8,17 @@ public abstract class Person {
     public abstract float eat();
 
     //Abstraction and extending (inheritance) are the basis of Object Oriented Programming (OOP)
+
+    //When you "inherit" from another class (see src.main.lib.people.types for examples of using the extend keyword)
+    //You inherit all if its variables and methods. Effectively, you use it as a basis for the new class.
+    //When a class inherits from another, it is considered a child of that other class,
+    //and the other class is called the parent.
+
+    //Using the abstract keyword for classes allows you to define abstract methods
+    //that are not implemented in the parent class, but MUST be implemented in the child class.
+    //You cannot actually create instances of abstract classes; they are only used as a basis
+    //for other classes to inherit from.
+
+    //For more information on OOP, I will again direct you to google;
+    //it is a very complicated topic that unfortunately I won't have the time to properly detail
 }
